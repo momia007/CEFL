bea48ef3946abb6a09c2f16ea83ce06f55fab2d2
unknown <martinchiappero@gmail.com>
2025-06-06 20:11:35 -0300

    Primer commit

---
+++ js/menu.js
@@ -0,0 +1,7 @@
+function alternarMenu() {
+    document.querySelector(".lista-barra").classList.toggle("active");
+}
+
+/* document.querySelector(".menu-toggle").addEventListener("click", function() {
+    document.querySelector(".lista-barra").classList.toggle("active");
+}); */
